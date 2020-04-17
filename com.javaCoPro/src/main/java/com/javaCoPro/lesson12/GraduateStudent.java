ff0da42cc4bfdd27f9a5c35fd7069b9f9bc8c68e
Denis <35831800+8Denis8@users.noreply.github.com>
2020-04-16 23:34:31 -0700

    age

--- com.javaCoPro/src/main/java/com/javaCoPro/lesson12/GraduateStudent.java
+++ com.javaCoPro/src/main/java/com/javaCoPro/lesson12/GraduateStudent.java
@@ -1,5 +1,10 @@
 package com.javaCoPro.lesson12;
 
-public class GraduateStudent {
+public class GraduateStudent extends Student {
+
+    public GraduateStudent(String name, String lastName, int age, String gender, Address address, String major,
+            String program) {
+        super(name, lastName, age, gender, address, major, program);
+    }
 
 }
